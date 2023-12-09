5faeee973db305be3127461254f48e5108809feb
333Kay333 <140438505+333Kay333@users.noreply.github.com>
2023-12-09 00:15:42 -0500

    #work on img

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ window.addEventListener('load', function() {
   const ctx = canvas.getContext('2d');
   canvas.width = 500;
   canvas.height = 500;
-  console.log(ctx);
+  
 
   class Mandrake {
     constructor(){
-this.image = document.getElementById('mandrake');
+        this.image = document.getElementById('mandrake');
+        this.spriteWidth = 256;
+        this.spriteHeight = 256;
+        this.width = this.spriteWidth;
+        this.height = this.spriteHeight;
+        this.x = 0;
+        this.y = 0;
+        this.minFrame = 0;
+        this.maxFrame = 355;
     }
-    draw(){
+    draw(context){
+context.drawImage(this.image, this.x, this.y, canvas.width, canvas.height);
+    }
+    update(){
 
     }
+  }
+  const mandrake = new Mandrake();
+
+  function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    mandrake.draw(ctx);
+    requestAnimationFrame(animate);
   }
 });
