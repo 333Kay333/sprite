86924a2d911e0ab49982631e1064cb126e3efc28
333Kay333 <140438505+333Kay333@users.noreply.github.com>
2023-12-16 11:21:59 -0500

    #its jumping

--- index.js
+++ index.js
@@ -14,23 +14,34 @@ window.addEventListener('load', function() {
         this.spriteHeight = 256;
         this.width = this.spriteWidth;
         this.height = this.spriteHeight;
-        this.x = this.canvasWidth/2;
-        this.y = this.canvasHeight/2;
+        this.scale = 2;
+        this.x = this.canvasWidth/2 - this.width * this.scale/2;
+        this.y = this.canvasHeight/2  - this.height * this.scale/2;
         this.minFrame = 0;
         this.maxFrame = 355;
+        this.frame = this.minFrame;
+        this.frameX = 3;
+        this.frameY = 10;
     }
     draw(context){
-        context.drawImage(this.image, 6 * this.spriteWidth, 8 * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width * this.scale, this.height * this.scale);
     }
     update(){
-
+       /* if (this.frameX < 17) this.frameX++;
+        else this.frameX = 0;*/
+        if (this.frame < this.maxFrame) this.frame++;
+        else this.frame = this.minFrame;
+        this.frameX = this.frame % 18;
+        this.frameY = Math.floor(this.frame / 18);
     }
   }
   const mandrake = new Mandrake(canvas.width, canvas.height);
 
   function animate(){
-   
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     mandrake.draw(ctx);
+    mandrake.update();
+    requestAnimationFrame(animate);
     
   }
   animate();
